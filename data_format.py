965e5ad97e6ae8234dcf1207c651b00da3b5bf62
RyanFcr <3200103626@zju.edu.cn>
2022-12-27 17:54:08 +0800

    final 3 points

---
+++ data_format.py
@@ -0,0 +1,14 @@
+f1 = open('./data/championsdata.csv', 'r')
+f2 = open('final.txt', 'a')
+firstline = f1.readline()
+att_name = firstline.split(',')
+# f2.write('[\'Date\',\'FG3A\'],\n')
+for line in f1.readlines()[::]:
+    token = line.split(',')
+    sdate = token[att_name.index('TPA')]
+    # f2.write('\"')
+    f2.write(sdate+',')
+    # f2.write(token[att_name.index('FG3A')])
+    # f2.write('],\n')
+    # line = f1.readline()
+f2.write('\n')
